32515e1ddf08ee1a7265f6af3345fb8d792fa9f2
Do Minh Quang <quangdm961@gmail.com>
2023-12-29 09:40:50 +0100

    latest

--- core/src/managers/PlayStateController.java
+++ core/src/managers/PlayStateController.java
@@ -12,16 +12,20 @@ public class PlayStateController {
     public static Texture card = new Texture("img/card.png");
     public static Texture avatar = new Texture("img/P2_avatar.png");
 
-    public static Texture special = new Texture("img/1.png");
+    public static Texture special = new Texture("img/special_1.png");
+
+    public static Texture leaf = new Texture("img/leaf_top_right_yellow.png");
 
     public static void draw(){
         Game.batch.draw(cordMap,Cords.cordMap_x,Cords.cordMap_y);
         Game.batch.draw(card,Cords.top_card_4_x,Cords.top_card_4_y);
         Game.batch.draw(avatar, Cords.top_avatar_x, Cords.top_avatar_y);
-        //Game.batch.draw(special,);
+        Game.batch.draw(special, Cords.special_4_x, Cords.special_4_y);
+        Game.batch.draw(leaf,0,0);
     }
 
     public static void dispose(){
+
         cordMap.dispose();
     }
 }
