fd4bbfb588fae2ebe66ee1c1c9f4d50429984c42
Do Minh Quang <quangdm961@gmail.com>
2023-12-29 11:02:37 +0100

    modded

--- core/src/managers/PlayStateController.java
+++ core/src/managers/PlayStateController.java
@@ -27,18 +27,50 @@ public class PlayStateController {
             i = i + 1;
         }
 
+        //---------- Leaves ------------------------
+
+
+        //---- Monkeys and elephant-----------------
+
+        //---- Monkeys and elephant-----------------
+
+
+        //-------- Player and cards-------------------
+
+        //-------- Player and cards-------------------
+
+
+        //------------ Special ----------------------
+
+        //------------ Special ----------------------
     }
 
     public static void draw(){
+
+        //---------- Leaves -----------------------
         Game.batch.draw(leaf,0,0);
+        //---------- Leaves ------------------------
+
+
+        //---------- Cord map ----------------------
         Game.batch.draw(cordMap,Cords.cordMap_x,Cords.cordMap_y);
+        //---------- Cord map ----------------------
+
+
+        //---- Monkeys and elephant-----------------
+        Game.batch.draw(monkey, Cords.cord[i][0],Cords.cord[i][1]);
+        //---- Monkeys and elephant-----------------
+
+
+        //-------- Player and cards-------------------
         Game.batch.draw(card,Cords.top_card_4_x,Cords.top_card_4_y);
         Game.batch.draw(avatar, Cords.top_avatar_x, Cords.top_avatar_y);
+        //-------- Player and cards-------------------
+
+
+        //------------ Special ----------------------
         Game.batch.draw(special, Cords.special_4_x, Cords.special_4_y);
-
-        Game.batch.draw(monkey, Cords.cord[i][0],Cords.cord[i][1]);
-
-
+        //------------ Special ----------------------
     }
 
     public static void dispose(){
